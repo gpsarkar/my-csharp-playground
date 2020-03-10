33a6111489c0f57229ef9be899279d4955781428
gpsarkar <gpsarkar@live.com>
2020-03-11 02:41:06 +0530

    just for fun

---
+++ caterpi431-ctm/Program.cs
@@ -0,0 +1,196 @@
+﻿// Author - Ganapati Sarkar
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+// Atm program 
+namespace caterpi431_ctm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var atm = new Atm();
+
+            atm.WithdrawMoney(500444);
+            atm.WithdrawMoney(31900);
+            atm.WithdrawMoney(200);
+            atm.WithdrawMoney(500); 
+            atm.WithdrawMoney(1000);
+            atm.WithdrawMoney(10);
+            //Console.WriteLine(atm.WithdrawMoney(-100)); // Not allowed as its an uint
+            atm.WithdrawMoney(100);
+            atm.WithdrawMoney(16321);
+            atm.WithdrawMoney(16321);
+            atm.WithdrawMoney(16321);
+            atm.WithdrawMoney(26300);
+            atm.WithdrawMoney(26300);
+            atm.WithdrawMoney(26300);
+            atm.WithdrawMoney(26300);
+            atm.WithdrawMoney(26300);
+            atm.WithdrawMoney(16300);
+            atm.WithdrawMoney(5300);
+            atm.WithdrawMoney(5300);
+            atm.WithdrawMoney(5600);
+            atm.WithdrawMoney(5600);
+            atm.WithdrawMoney(26300);
+        }
+    }
+
+
+    public interface ICurrency : IComparable
+    {
+        ushort Value { get; }
+    }
+
+    public abstract class CurrencyBase : ICurrency
+    {
+        public abstract ushort Value { get; }
+        
+        public virtual int CompareTo(object obj)
+        {
+            if (!(obj is ICurrency currency)) return -1;
+            var val = currency.Value;
+
+            if (val > Value) return 1;
+            if (val < Value) return -1;
+            return 0;
+        }
+    }
+
+    public class Currency1000 : CurrencyBase
+    {
+        public override ushort Value => 1000;
+    }
+    
+    public class Currency500 : CurrencyBase
+    {
+        public override ushort Value => 500;
+    }
+
+    public class Currency100 : CurrencyBase
+    {
+        public override ushort Value => 100;
+    }
+
+
+    // Not required.. just for extending the atm currency slots
+    public class Currency50 : CurrencyBase
+    {
+        public override ushort Value => 50;
+    }
+
+    public class Currency20 : CurrencyBase
+    {
+        public override ushort Value => 20;
+    }
+
+    public class Currency10 : CurrencyBase
+    {
+        public override ushort Value => 10;
+    }
+
+    //-----------------
+
+
+    public interface IAtm
+    {
+        SortedDictionary<ICurrency, uint> Currencies { get; set; }
+
+        uint AtmBalance { get; }
+
+        SortedDictionary<ICurrency, uint> WithdrawMoney(uint amount);
+    }
+
+    public class Atm : IAtm
+    {
+        public Atm()
+        {
+            // Initialize the atm currencies
+            //Currencies = new SortedDictionary<ICurrency, uint>()
+            //{
+            //    [new Currency1000()] = 50,
+            //    [new Currency500()] = 100,
+            //    [new Currency100()] = 1000
+            //};
+
+            Currencies = new SortedDictionary<ICurrency, uint>()
+            {
+                [new Currency1000()] = 50,
+                [new Currency500()] = 100,
+                [new Currency100()] = 1000,
+                [new Currency50()] = 1000,
+                [new Currency20()] = 1000,
+                [new Currency10()] = 1000
+            };
+        }
+
+        public SortedDictionary<ICurrency, uint> Currencies { get; set; }
+
+        public uint AtmBalance =>
+            Currencies.Aggregate<KeyValuePair<ICurrency, uint>, uint>(0, (current, currency) 
+                => current + currency.Key.Value * currency.Value);
+
+        public SortedDictionary<ICurrency, uint> WithdrawMoney(uint amount)
+        {
+            var tempAmount = amount;
+
+            SortedDictionary<ICurrency, uint> withdrawnCurrencies = new SortedDictionary<ICurrency, uint>();
+
+            if (tempAmount > AtmBalance)
+            {
+                Console.WriteLine($"WITHDRAW:{amount}. Cannot withdraw. ATM is poorer than you !");
+
+                return withdrawnCurrencies;
+            }
+
+
+            // Get withdrawn currencies
+            foreach (var currency in Currencies.ToDictionary(c => c.Key, c => c.Value))
+            {
+                var noOfNotes = Math.DivRem((int) tempAmount, (int) currency.Key.Value, out var balance);
+
+                if (noOfNotes < currency.Value)
+                {
+                    withdrawnCurrencies.Add(currency.Key, (uint) noOfNotes);
+                    tempAmount = (uint) balance;
+
+                    // Update the currencies
+                    Currencies[currency.Key] = Currencies[currency.Key] - (uint)noOfNotes;
+                }
+                else
+                {
+                    withdrawnCurrencies.Add(currency.Key, currency.Value);
+                    tempAmount = amount - (currency.Value * currency.Key.Value);
+
+                    // Empty the coffers
+                    Currencies[currency.Key] = 0;
+                }
+            }
+
+
+            // Print just for sake
+
+            Console.WriteLine($"WITHDRAWN:{amount}");
+            foreach (var keyValuePair in withdrawnCurrencies)
+            {
+                Console.Write($"{keyValuePair.Key.Value}:{keyValuePair.Value}, ");
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("ATM BALANCE");
+            foreach (var keyValuePair in Currencies)
+            {
+                Console.Write($"{keyValuePair.Key.Value}:{keyValuePair.Value}, ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+            return withdrawnCurrencies;
+        }
+    }
+
+}
---
+++ csharp6-features/csharp6-features/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace csharp6_features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/exploration/csharp-6?tutorial-step=1
+            var p = new Person("Bill", "Wagner");
+            WriteLine($"The name, in all caps: {p.AllCaps()}");
+            WriteLine($"The name is: {p}");
+
+
+            //https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/exploration/csharp-6?tutorial-step=5
+            var phrase = "the quick brown fox jumps over the lazy dog";
+            var wordLength = from word in phrase.Split(' ') select word.Length;
+            WriteLine($"The average word length is: {wordLength.Average()}");
+            WriteLine($"The average word length is: {wordLength.Average():F2}");
+
+
+            // https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/exploration/csharp-6?tutorial-step=6
+            string s = null;
+            bool hasMore = s?.ToCharArray()?.GetEnumerator()?.MoveNext() ?? false;
+            WriteLine(hasMore);
+
+
+            //https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/exploration/csharp-6?tutorial-step=7
+            try
+            {
+                string s1 = null;
+                WriteLine(s1.Length);
+
+            }
+            catch (Exception e) when (LogException(e))
+            {
+            }
+            WriteLine("Exception must have been handled");
+
+
+            // https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/exploration/csharp-6?tutorial-step=8
+            WriteLine(nameof(System.String));
+            int j = 5;
+            WriteLine(nameof(j));
+            List<string> names = new List<string>();
+            WriteLine(nameof(names));
+
+            WriteLine(nameof(p));
+            WriteLine(nameof(p.FirstName));
+            WriteLine(nameof(p.AllCaps));
+
+
+            // https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/exploration/csharp-6?tutorial-step=9
+            var messages = new Dictionary<int, string>
+            {
+                [404] = "Page not Found",
+                [302] = "Page moved, but left a forwarding address.",
+                [500] = "The web server can't come out to play today."
+            };
+            WriteLine(messages[302]);
+
+            var capitals = new Dictionary<string, string>
+            {
+                ["india"] = "New Delhi",
+                ["china"] = "Beijing",
+                ["germany"] = "Berlin"
+            };
+
+            //WriteLine($"india1234 ---- {capitals["india1234"]}");
+            WriteLine($"germany ---- {capitals["Germany".ToLower()]}");
+        }
+
+
+
+        private static bool LogException(Exception e)
+        {
+            WriteLine($"\tIn the log routine. Caught {e.GetType()}");
+            WriteLine($"\tMessage: {e.Message}");
+            return true; // <----- return false to not handle exception
+        }
+    }
+
+
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Person(string first, string last)
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
+        public override string ToString() => $"{FirstName} {LastName}";
+
+        public string AllCaps() => ToString().ToUpper();
+    }
+}
